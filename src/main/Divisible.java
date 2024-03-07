835c8d2f8c61a75f15880100e1e27c0647e025f3
HamiDuong <hd028@hdm-stuttgart.de>
2024-03-07 17:01:31 +0100

    restructer for source and test folder

--- src/main/Divisible.java
+++ src/main/Divisible.java
@@ -1,19 +1,19 @@
-package aufgabe3;
-
 import java.util.Scanner;
 public class Divisible {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Which number to check for parity?");
+        System.out.println("Which number to check for parity?");
         int number = scanner.nextInt();
 
         boolean result = checkDivisibility(number);
 
         if(result){
-            System.out.print(number + " is even");
+            System.out.println(number + " is even");
         } else {
-            System.out.print(number + " is odd");
+            System.out.println(number + " is odd");
         }
+
+        //System.out.println((result)?number + " is even":number + " is odd");
 
         scanner.close();
     }
--- src/test/DivisibleTest.java
+++ src/test/DivisibleTest.java
@@ -1,15 +1,14 @@
-package aufgabe3;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static aufgabe3.Divisible.checkDivisibility;
+
 public class DivisibleTest {
     @Test
     public void testEven() {
-        assertTrue(checkDivisibility(10));
+        assertTrue(Divisible.checkDivisibility(10));
     }
 
     @Test
     public void testOdd() {
-        assertFalse(checkDivisibility(5));
+        assertFalse(Divisible.checkDivisibility(5));
     }
 }
