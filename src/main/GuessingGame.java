3aca6f42384969f6282a15ac543675806b63c64c
HamiDuong <hd028@hdm-stuttgart.de>
2024-03-08 13:23:27 +0100

    added task 6: guessing game

---
+++ src/main/GuessingGame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Random rand = new Random();
+        int randNum = rand.nextInt(100);
+
+        System.out.println("Try to guess the number between 0 - 100");
+        int num = scanner.nextInt();
+
+        int passes = 0;
+
+        while(randNum != num){
+            passes++;
+            if(randNum < num){
+                System.out.println("Your guess is to high");
+            }else{
+                System.out.println("Your guess is to low");
+            }
+            System.out.println("Try to guess again:");
+            num = scanner.nextInt();
+        }
+
+        System.out.println("The number was "+ randNum +". You correctly guessed the number in "+ passes +" tries");
+
+        scanner.close();
+    }
+}
