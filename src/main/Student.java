fb370067c83afc4f058411df1b4946afac5b590f
HamiDuong <hd028@hdm-stuttgart.de>
2024-03-13 10:26:45 +0100

    created subclass student to person for testing purpose

---
+++ src/main/Student.java
@@ -0,0 +1,5 @@
+public class Student extends Person{
+    public Student(Integer age, String firstname, String lastname, Integer height, Integer weight) {
+        super(age, firstname, lastname, height, weight);
+    }
+}
