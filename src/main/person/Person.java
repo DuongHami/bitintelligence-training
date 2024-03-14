3f941f88e1c8ed8e729c9f5f8a301bc8439f0f53
HamiDuong <hd028@hdm-stuttgart.de>
2024-03-14 16:35:51 +0100

    using Lombok Annotation to shorten code

--- src/main/person/Person.java
+++ src/main/person/Person.java
@@ -1,88 +1,16 @@
 package person;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Person implements Comparable<Person> {
     private Integer age;
     private String firstname;
     private String lastname;
     private Integer height;
     private Integer weight;
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-
-    public Integer getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Integer weight) {
-        this.weight = weight;
-    }
-
-    public Person(Integer age, String firstname, String lastname, Integer height, Integer weight) {
-        this.age = age;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.height = height;
-        this.weight = weight;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Person person)) return false;
-        return  Objects.equals(age, person.age) &&
-                Objects.equals(firstname, person.firstname) &&
-                Objects.equals(lastname, person.lastname) &&
-                Objects.equals(height, person.height) &&
-                Objects.equals(weight, person.weight);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(age, firstname, lastname, height, weight);
-    }
-
-    @Override
-    public String toString() {
-        return "person.Person{" +
-                "age=" + age +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", height=" + height +
-                ", weight=" + weight +
-                '}';
-    }
 
     @Override
     public int compareTo(Person o) {
